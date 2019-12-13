bb3dd155390ecf63eb294789740097fab3453ffc
Aram Taieb <aram.taieb@combination.se>
2019-12-13 15:49:30 +0100

    - Started working on Day 5 - IntCode program is now a separate lib

---
+++ IntCode/IntCode.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+
+namespace IntCode
+{
+    public static class IntCode
+    {
+        public static string CalcOpCode(string input)
+        {
+            string[] tokens = input.Split(',');
+            int[] values = tokens.Select(s => int.Parse(s)).ToArray();
+
+            for (int i = 0; i < values.Length; i += 4)
+            {
+                int opCode = values[i];
+
+                if (opCode == 99)
+                {
+                    break;
+                }
+
+                int pos1 = values[i + 1];
+                int pos2 = values[i + 2];
+                int resultPos = values[i + 3];
+
+                if (opCode == 1)
+                {
+                    values[resultPos] = values[pos1] + values[pos2];
+                }
+                else if (opCode == 2)
+                {
+                    values[resultPos] = values[pos1] * values[pos2];
+                }
+                else
+                {
+                    Console.WriteLine($"Unkown opcode detected: {opCode}, aborting: ");
+                }
+
+            }
+
+            return string.Join(",", values);
+        }
+
+        public static int CalcOpCodeWithNounAndVerb(string input, int noun, int verb)
+        {
+            string[] tokens = input.Split(',');
+            int[] values = tokens.Select(s => int.Parse(s)).ToArray();
+            values[1] = noun;
+            values[2] = verb;
+
+            for (int i = 0; i < values.Length; i += 4)
+            {
+                int opCode = values[i];
+
+                if (opCode == 99)
+                {
+                    break;
+                }
+
+                int pos1 = values[i + 1];
+                int pos2 = values[i + 2];
+                int resultPos = values[i + 3];
+
+                if (opCode == 1)
+                {
+                    values[resultPos] = values[pos1] + values[pos2];
+                }
+                else if (opCode == 2)
+                {
+                    values[resultPos] = values[pos1] * values[pos2];
+                }
+                else
+                {
+                    Console.WriteLine($"Unkown opcode detected: {opCode}, aborting: ");
+                }
+
+            }
+
+            return values[0];
+        }
+    }
+}
