6a90092eb9cb89fc774732d7679f1e8f28f0d4c2
Aram Taieb <aram.taieb@combination.se>
2019-12-13 14:51:53 +0100

    Day 3 part 2 WIP

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -13,9 +13,14 @@ namespace Day3
             var test1 = new string[2] { "R75, D30, R83, U83, L12, D49, R71, U7, L72", "U62,R66,U55,R34,D71,R55,D58,R83" };
             var test2 = new string[2] { "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" };
 
+            // Part 1
             Console.WriteLine(CalcShortestDistance(test1));
             Console.WriteLine(CalcShortestDistance(test2));
             Console.WriteLine(CalcShortestDistance(input1));
+
+            // Part 2
+            // TODO: find intersection with least amount of steps for both wires
+
             Console.ReadKey();
         }
 
