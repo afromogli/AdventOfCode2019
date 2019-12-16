fd144e6a9e73e90f70db79124ed16d9517d066aa
Aram Taieb <aram.taieb@combination.se>
2019-12-16 17:16:21 +0100

    Day 7 WIP

--- Day7/Day7.cs
+++ Day7/Day7.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Day7
 {
@@ -10,6 +7,32 @@ namespace Day7
     {
         static void Main(string[] args)
         {
+            var input1 = File.ReadAllLines(@"input1.txt")[0];
+            var test1 = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0";
+            var test1PhaseSettingSequence = new int[]
+            {
+                4,3,2,1,0
+            };
+            int highestCollectedThrusterSignal = 0;
+
+            int[] amplifierOutputs = new int[5];
+            int[] phaseSettingSequence = test1PhaseSettingSequence;
+
+
+            for (int i = 0; i < amplifierOutputs.Length; i++)
+            {
+                int input = i == 0 ? 0 : amplifierOutputs[i-1]; 
+                amplifierOutputs[i] = IntCode.IntCode.CalcOpCodeV4(test1, phaseSettingSequence[i]);
+                Console.Write(input);
+            }
+
+            highestCollectedThrusterSignal = amplifierOutputs[4];
+
+            Console.WriteLine(highestCollectedThrusterSignal);
+
+            //IntCode.IntCode.CalcOpCodeV3(input1);
+
+            Console.ReadKey();
         }
     }
 }
--- IntCode/IntCode.cs
+++ IntCode/IntCode.cs
@@ -240,6 +240,167 @@ namespace IntCode
             return values[0];
         }
 
+        // Day 7
+        public static int CalcOpCodeV4(string input, int phaseSetting)
+        {
+            int[] values = input.Split(',').Select(s => int.Parse(s)).ToArray();
+
+            int opStepSize = 4;
+            for (int i = 0; i < values.Length; i += opStepSize)
+            {
+                string instr = values[i].ToString();
+                int opCode = i == 0 ? phaseSetting : (instr.Length == 1 ? values[i] : int.Parse(instr.Substring(instr.Length - 2)));
+                int paramMode1 = instr.Length > 2 ? int.Parse(instr.Substring(instr.Length - 3, 1)) : 0;
+                int paramMode2 = instr.Length > 3 ? int.Parse(instr.Substring(instr.Length - 4, 1)) : 0;
+                int paramMode3 = instr.Length > 4 ? int.Parse(instr.Substring(instr.Length - 5, 1)) : 0;
+
+                if (opCode == 99)
+                {
+                    break;
+                }
+
+                switch (opCode)
+                {
+                    // addition
+                    // multiplication
+                    case 1:
+                    case 2:
+                        {
+                            int param1 = values[i + 1];
+                            int param2 = values[i + 2];
+                            int resultAddress = values[i + 3];
+
+                            int res;
+                            int param1Val = GetParamValueByParamMode(paramMode1, values, param1);
+                            int param2Val = GetParamValueByParamMode(paramMode2, values, param2);
+
+                            if (opCode == 1)
+                            {
+                                res = param1Val + param2Val;
+                            }
+                            else
+                            {
+                                res = param1Val * param2Val;
+
+                            }
+                            SetMem(values, resultAddress, res);
+                            opStepSize = 4;
+                            break;
+                        }
+
+                    // take input and store at address 
+                    case 3:
+                        {
+                            int address = values[i + 1];
+
+                            int inputVal = int.Parse(Console.ReadLine());
+                            SetMem(values, address, inputVal);
+                            opStepSize = 2;
+                            break;
+                        }
+                    // output value stored at address
+                    case 4:
+                        {
+                            int param1 = values[i + 1];
+                            if (paramMode1 == 0)
+                            {
+                                // Output value from address X
+                                Console.WriteLine(values[param1]);
+                            }
+                            else
+                            {
+                                // Output value
+                                Console.WriteLine(param1);
+                            }
+
+                            opStepSize = 2;
+                            break;
+                        }
+                    // jump-if-true
+                    // jump-if-false
+                    case 5:
+                    case 6:
+                        {
+                            int param1 = values[i + 1];
+                            int param2 = values[i + 2];
+
+                            int param1Val = GetParamValueByParamMode(paramMode1, values, param1);
+                            int param2Val = GetParamValueByParamMode(paramMode2, values, param2);
+
+                            // jump if true
+                            if (opCode == 5)
+                            {
+                                if (param1Val != 0)
+                                {
+                                    i = param2Val;
+                                    opStepSize = 0;
+                                    break;
+                                }
+                            }
+                            // jump if false
+                            else
+                            {
+                                if (param1Val == 0)
+                                {
+                                    i = param2Val;
+                                    opStepSize = 0;
+                                    break;
+                                }
+                            }
+                            opStepSize = 3;
+                            break;
+                        }
+                    // less than
+                    // equals
+                    case 7:
+                    case 8:
+                        {
+                            int param1 = values[i + 1];
+                            int param2 = values[i + 2];
+                            int resultAddress = values[i + 3];
+
+                            int res;
+                            int param1Val = GetParamValueByParamMode(paramMode1, values, param1);
+                            int param2Val = GetParamValueByParamMode(paramMode2, values, param2);
+
+                            if (opCode == 7)
+                            {
+                                if (param1Val < param2Val)
+                                {
+                                    res = 1;
+                                }
+                                else
+                                {
+                                    res = 0;
+                                }
+                            }
+                            else
+                            {
+                                if (param1Val == param2Val)
+                                {
+                                    res = 1;
+                                }
+                                else
+                                {
+                                    res = 0;
+                                }
+                            }
+
+                            SetMem(values, resultAddress, res);
+                            opStepSize = 4;
+
+                            break;
+                        }
+                    default:
+                        Console.WriteLine($"Unkown opcode detected: {opCode}, aborting..");
+                        break;
+                }
+
+            }
+
+            return values[0];
+        }
+
         private static void SetMem(int[] mem, int address, int value)
         {
             mem[address] = value;
